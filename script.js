b87437ddcf9d455e4ea8e23682f7b6ad2a2d03dc
adoydevs <imranadoy@gmail.com>
2023-09-27 16:53:28 +0600

    add

---
+++ script.js
@@ -0,0 +1,47 @@
+// let counter = 0;
+// function count(){
+//     counter++;
+//     alert(counter);
+// }
+
+
+// function myCar (carName , carWeight, carColor, carSeat){
+//     this.carName = carName,
+//     this.carWeight = carWeight,
+//     this.carColor = carColor,
+//     this.carSeat = carSeat
+
+//     this.display = function(){
+//         console.log(this.carName);
+//         console.log(this.carWeight);
+//         console.log(this.carColor);
+//         console.log(this.carSeat);
+//     }
+
+// }
+
+// let myCarDetails = new myCar ("Bhugati" , "500kg" , "blue" , 10);
+// let yourCar = new myCar ("You" , "100kg" , "red" , 50);
+
+// myCarDetails.display();
+// yourCar.display();
+
+
+// let x = 5;
+// x--;
+// console.log(x);
+
+
+// let counTer = Number (prompt("Type a Number"));
+// let result = counTer > 0 ? "Positive" :  counTer < 0 ? "Negetive" : "Zero";
+
+// console.log(result);
+
+// let txt = "Hello World!";
+// txt = txt.toUpperCase();
+
+
+function myCar(a , b){
+    return a + b;
+}
+
